07c8d210ce0107df07d6baa17bc45c3142f40e5a
uitox_macbook <JerryLiao@uitox.com>
2016-03-23 17:20:53 +0800

    Initial Commit

---
+++ UXConnectKit/UXConnectKit.h
@@ -0,0 +1,19 @@
+//
+//  UXConnectKit.h
+//  UXConnectKit
+//
+//  Created by uitox_macbook on 2016/3/23.
+//  Copyright © 2016年 uitox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UXConnectKit.
+FOUNDATION_EXPORT double UXConnectKitVersionNumber;
+
+//! Project version string for UXConnectKit.
+FOUNDATION_EXPORT const unsigned char UXConnectKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UXConnectKit/PublicHeader.h>
+
+
